04b622c7ab3b62e68cbc524baa04b1f16dd4f3b3
kalynbeach <kalynbeach@gmail.com>
2022-08-20 14:57:57 -0700

    initial commit, add basic web_audio_api AudioBuffer test

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use web_audio_api::context::{AudioContext, BaseAudioContext};
+use web_audio_api::node::{AudioNode, AudioScheduledSourceNode};
+
+fn main() {
+    println!("\n w a v e _ t o o l s \n");
+
+    let file_path = String::from("samples/test_beat.wav");
+
+    // Set up audio context
+    let context = AudioContext::default();
+
+    // Open local file and create an audio buffer
+    let file = File::open(file_path).unwrap();
+    let buffer = context.decode_audio_data_sync(file).unwrap();
+
+    // Play the buffer at given volume
+    let volume = context.create_gain();
+    volume.connect(&context.destination());
+    volume.gain().set_value(0.5);
+
+    let buffer_source = context.create_buffer_source();
+    buffer_source.connect(&volume);
+    buffer_source.set_buffer(buffer);
+
+    // Start the source
+    buffer_source.start();
+
+    // Listen
+    std::thread::sleep(std::time::Duration::from_secs(4));
+}
