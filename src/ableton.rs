21c27c6feaecb36a95f10cc1d49aa838ad42a249
kalynbeach <kalynbeach@gmail.com>
2022-08-27 14:42:39 -0700

    add ableton structs to build out later

--- src/ableton.rs
+++ src/ableton.rs
@@ -1,4 +1,16 @@
+// use chrono::prelude::*;
+
 // pub struct Project {
 //     title: String,
 //     artist: String,
+// }
+
+// pub struct Set {
+//     title: String,
+//     created: DateTime<Local>,
+//     modified: DateTime<Local>,
+// }
+
+// pub struct Clip {
+//     name: String,
 // }
