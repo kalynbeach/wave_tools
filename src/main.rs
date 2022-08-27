53c0e5951b0f0ac954e4ac4eef5d7ef8b7dd18e8
kalynbeach <kalynbeach@gmail.com>
2022-08-27 14:41:39 -0700

    add initial clap CLI struct and commands enum

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
 pub mod audio;
 pub mod ableton;
 pub mod elektron;
 pub mod music;
 
+// See: https://docs.rs/clap/latest/clap/_derive/_tutorial/index.html
+
+#[derive(Parser)]
+#[clap(name = "WaveTools")]
+#[clap(author = "Kalyn Beach")]
+#[clap(version = "0.1.0")]
+#[clap(about = "Audio and music production tools", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+    #[clap(short, long, value_parser, value_name = "FILE")]
+    file_path: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // Calls test_audio
+    Test { name: Option<String> }
+}
+
 fn main() {
     println!("\n w a v e _ t o o l s \n");
 
-    let context = audio::create_audio_context();
-    let file_path = String::from("samples/test_beat.wav");
+    let cli = Cli::parse();
 
-    audio::test_audio(&context, file_path);
+    match &cli.command {
+        Commands::Test { name } => {
+            println!("Test file name is: {:?}", name);
+            let context = audio::create_audio_context();
+            let file_path = String::from("samples/test_beat.wav");
+            audio::test_audio(&context, file_path);
+        }
+    }
+
+    // let context = audio::create_audio_context();
+    // let file_path = String::from("samples/test_beat.wav");
+    // audio::test_audio(&context, file_path);
 }
